cf61aee678f2c370ff091d73ae31233dd254c5d2
vfadeel <76406063+vfadeel@users.noreply.github.com>
2022-04-27 14:16:08 -0300

    c#

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace UserSecrets
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //Comentario testando o github.dev
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
